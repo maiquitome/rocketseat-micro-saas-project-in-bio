b700c39bd5e48aa45f1ca3e29a77ed26c7320eae
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-12-22 12:00:07 -0300

    feat: :sparkles: add Hero section initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
-import Image from "next/image";
+import Hero from "./components/landing-page/hero";
 
 export default function Home() {
   return (
-    <div className="bg-background-primary text-content-body">Hello World</div>
+    <div className="max-w-7xl mx-auto border">
+      <Hero />
+      {/* 
+      <Header />
+      <Hero />
+      <VideoExplanation />
+      <Pricing />
+      <FAQ />
+      <Footer /> 
+      */}
+    </div>
   );
 }
